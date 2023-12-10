d2ac16a9cafc11b61f5e146719a118e492529a9c
Alex Pirko <youzhny.foto@gmail.com>
2023-12-10 13:25:15 +0200

    add Skills component

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,7 @@ import Head from 'next/head';
 import React, { useEffect, useRef } from 'react';
 import Image from 'next/image';
 import { useInView, useMotionValue, useSpring } from 'framer-motion';
+import Skills from '@/components/Skills';
 
 const AnimatedNum = ({ value }) => {
     const ref = useRef(null);
@@ -109,6 +110,7 @@ const about = () => {
                             </div>
                         </div>
                     </div>
+                    <Skills/>
                 </Layout>
             </main>
         </>
